728c104581ce38ba45027f3715370fa5b30e1708
Mirlan Rysbekov <rys5ek0v.111@gmail.com>
2026-01-28 12:16:37 +0900

    Added cors policy

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 import mainRouter from './src/routes/index.js'
 
 
 const app = express()
+
+const allowedOrigins = ['http://localhost:5173', 'https://www.your-frontend-domain.com'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true // Enable passing of cookies, authorization headers, etc.
+}));
 
 
 app.use(express.json())
